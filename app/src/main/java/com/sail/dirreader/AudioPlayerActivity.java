baafb285e411921c6864792bf902868692e01e8c
RIchard O'Beirne <rjob.obeirne@gmail.com>
2021-11-15 09:30:10 +1100

    Added mediaPlayer
    successfully plays  playlist sequentially

--- app/src/main/java/com/sail/dirreader/AudioPlayerActivity.java
+++ app/src/main/java/com/sail/dirreader/AudioPlayerActivity.java
@@ -22,9 +22,12 @@ import java.util.TimerTask;
 public class AudioPlayerActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
-    AudioModel audio;
     SeekBar seekBar;
-
+    String mTrack;
+    Integer index, maxIndex;
+    Integer currentIndex = 0;
+    ArrayList<String> playList;
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,75 +35,102 @@ public class AudioPlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_audio_player);
 
         Intent intent = getIntent();
+        timer = new Timer();
 
-        ArrayList<String> playList = intent.getStringArrayListExtra("filepath");
+        playList = intent.getStringArrayListExtra("filepath");
         Log.e("player-path :", String.valueOf(playList));
 
-        for (int index = 0; index < playList.size(); index++) {
-            String playTrack = (String) playList.get(index);
-            Log.e("play-track", playTrack);
-        }
+        mediaPlayer = MediaPlayer.create(this, Uri.parse(playList.get(0)));
+        mediaPlayer.start();
 
-
-        audio = (AudioModel) intent.getSerializableExtra("playList");
+        if (playList.size() > 1) playNext();
+            index = 0;
+            maxIndex = playList.size();
+            mTrack = (String) playList.get(index);
+            Log.e("play-track", mTrack);
 
         TextView audioName = findViewById(R.id.audioName);
         Button playBtn = findViewById(R.id.playBtn);
         Button viewAllMediaBtn = findViewById(R.id.viewAllMedia);
 
-        if(audio!=null){
-            audioName.setText(audio.getaName());
-        }else{
-            audioName.setText("Piano.wav");
-        }
-
-
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startPlaying();
-            }
-        });
-
-        viewAllMediaBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopPlaying();
+//        if(audio!=null){
+//            audioName.setText(audio.getaName());
+//        }else{
+//            audioName.setText("Piano.wav");
+//        }
+//
+//
+//        playBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mMediaPlayer.start();
+//                startPlaying(playList);
+//            }
+//        });
+//
+//        viewAllMediaBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                stopPlaying();
 //                startActivity(new Intent(getApplicationContext(),MusicListActivity.class));
-                //finish();
-            }
-        });
+//                //finish();
+//            }
+//        });
+//
+//
 
-    }
+    }  // end of onCreate
+//
+//    public void playTrack(String mTrack) {
+//            final MediaPlayer mediaPlayer = MediaPlayer.create(this, Uri.parse(mTrack));
+//
+//
+//    }
+//
+//    public void startPlaying(String playTrack){
+//        if(mediaPlayer!=null && mediaPlayer.isPlaying()){
+//            mediaPlayer.stop();
+//        }
+//
+//        if(audio!=null){
+//            Toast.makeText(getApplicationContext(),"Playing from device; "+audio.getaPath(), Toast.LENGTH_SHORT).show();
+//               mediaPlayer = MediaPlayer.create(AudioPlayerActivity.this, Uri.parse(playTrack));
+//               mediaPlayer.start();
+//
+//           }
+//
+//        }else{
+//
+//            Toast.makeText(getApplicationContext(),"Playing from app", Toast.LENGTH_SHORT).show();
+//            try {
+//                AssetFileDescriptor afd = getAssets().openFd("piano.wav");
+//                mediaPlayer = new MediaPlayer();
+//                mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+//                afd.close();
+//                mediaPlayer.prepare();
+//                mediaPlayer.start();
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        enableSeekBar();
+//
+//    }
 
-    public void startPlaying(){
-        if(mediaPlayer!=null && mediaPlayer.isPlaying()){
-            mediaPlayer.stop();
-        }
-
-        if(audio!=null){
-
-            Toast.makeText(getApplicationContext(),"Playing from device; "+audio.getaPath(), Toast.LENGTH_SHORT).show();
-            mediaPlayer = MediaPlayer.create(AudioPlayerActivity.this, Uri.parse(audio.getaPath()));
-            mediaPlayer.start();
-
-        }else{
-
-            Toast.makeText(getApplicationContext(),"Playing from app", Toast.LENGTH_SHORT).show();
-            try {
-                AssetFileDescriptor afd = getAssets().openFd("piano.wav");
-                mediaPlayer = new MediaPlayer();
-                mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-                afd.close();
-                mediaPlayer.prepare();
+    public void playNext() {
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mediaPlayer.reset();
+                mediaPlayer = MediaPlayer.create(AudioPlayerActivity.this, Uri.parse(playList.get(++index)));
                 mediaPlayer.start();
-
-            } catch (Exception e) {
-                e.printStackTrace();
+                if (playList.size() > index+1) {
+                    playNext();
+                }
             }
-        }
-
-        enableSeekBar();
+        },mediaPlayer.getDuration()+100);
     }
 
     public void stopPlaying(){
